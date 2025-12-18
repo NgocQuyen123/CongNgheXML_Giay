69c791f0e05a1a71b728361cc7986aa13c82dc13
Ngoc Quyen <huynhngocquyen03042005@gmail.com>
2025-12-19 05:00:56 +0700

    update Ui form1 xóa button đăng ký

--- QuanLyShopGiay/Form1.Designer.cs
+++ QuanLyShopGiay/Form1.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.btnMain = new System.Windows.Forms.Button();
-            this.btnDangKy = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -43,32 +41,11 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // btnMain
-            // 
-            this.btnMain.Location = new System.Drawing.Point(457, 132);
-            this.btnMain.Name = "btnMain";
-            this.btnMain.Size = new System.Drawing.Size(94, 52);
-            this.btnMain.TabIndex = 1;
-            this.btnMain.Text = "main";
-            this.btnMain.UseVisualStyleBackColor = true;
-            this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
-            // 
-            // btnDangKy
-            // 
-            this.btnDangKy.Location = new System.Drawing.Point(161, 275);
-            this.btnDangKy.Name = "btnDangKy";
-            this.btnDangKy.Size = new System.Drawing.Size(166, 81);
-            this.btnDangKy.TabIndex = 2;
-            this.btnDangKy.Text = "Đăng ký";
-            this.btnDangKy.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnDangKy);
-            this.Controls.Add(this.btnMain);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -80,8 +57,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnMain;
-        private System.Windows.Forms.Button btnDangKy;
     }
 }
 
